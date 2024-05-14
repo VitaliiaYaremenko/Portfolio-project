d6763832ff96feef1c87dc1aaef5934b97d155d4
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-05-14 21:02:36 +0200

    created Project Hlegal

---
+++ app/js/main.js
@@ -0,0 +1,60 @@
+(() => {
+    const headerBurgerBtn = document.querySelector('.header__box-burger')
+    const headerNavigation = document.querySelector('.header__nav')
+    const headerCloseBtn = document.querySelector('.header__nav-close')
+
+    headerBurgerBtn.addEventListener('click', () => {
+        headerNavigation.classList.add('active')
+    })
+
+    headerCloseBtn.addEventListener('click', () => {
+        headerNavigation.classList.remove('active')
+    })
+})();
+
+(() => {
+    const tabsItems = document.querySelectorAll('.tabs__lists-item')
+    const contentsItems = document.querySelectorAll('.tabs__contents-item')
+
+    tabsItems.forEach((tab, index) => {
+        tab.addEventListener('click', e => {
+            contentsItems.forEach(content => {
+                content.classList.remove('active')
+            })
+            tabsItems.forEach(tab => {
+                tab.classList.remove('active')
+            })
+            tabsItems[index].classList.add('active')
+            contentsItems[index].classList.add('active')
+        })
+    })
+})();
+
+
+const teamslaider = document. querySelector ('.team__swiper')
+if (teamslaider) {
+    const teamswiper = new Swiper('.team__swiper', {
+        direction: 'horizontal',
+        spaceBetween: 30,
+        pagination: {
+            el: '.team__swiper-pagination',
+        },
+    });
+}
+
+const swiper = new Swiper('.about__swiper', {
+    loop: false,
+    slidesPerView: 1,
+    pagination: {
+        el: '.about__swiper-pagination',
+        type: "bullets",
+    },
+    breakpoints: {
+        // desktop >= 991
+        648: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        }
+    }
+});
+
---
+++ gulpfile.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const { src, dest, watch, parallel, series } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const fontFormat = require('gulp-fonter');
+const fontsFormat = require('gulp-ttf2woff2');
+const include = require('gulp-include');
+const postcss = require('gulp-postcss');
+const url = require('postcss-url');
+const path = require('path');
+const del = require('del');
+
+function compileStyles() {
+    return src([
+        'node_modules/swiper/swiper-bundle.css',
+        'app/scss/style.scss'
+    ])
+        .pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
+        // .pipe(postcss([
+        //     url({
+        //         url: 'copy',
+        //         assetsPath: path.join(__dirname, 'build/images'),
+        //         useHash: true,
+        //         basePath: path.join(__dirname, 'app')
+        //     })
+        // ]))
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 3 versions'] }))
+        .pipe(concat('style.min.css'))
+        .pipe(dest('app/css/'))
+        .pipe(browserSync.stream());
+}
+
+function includePages() {
+    return src('app/pages/*.html')
+        .pipe(include({
+            includePaths: 'app/components',
+        }))
+        .pipe(dest('app/'))
+        .pipe(browserSync.stream());
+}
+
+function fonts() {
+    return src('app/fonts/*.*')
+        .pipe(fontFormat({
+            formats: ['woff', 'ttf']
+        }))
+        .pipe(src('app/fonts/*.ttf'))
+        .pipe(fontsFormat())
+        .pipe(dest('build/fonts/'));
+}
+
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(dest('build/images'))
+        .pipe(webp())
+        .pipe(dest('build/images'));
+}
+
+function script() {
+    return src([
+        'node_modules/swiper/swiper-bundle.js',
+        'app/js/*.js',
+        '!app/js/main.min.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js/'))
+        .pipe(browserSync.stream());
+}
+
+function watching() {
+    browserSync.init({
+        server: {
+            baseDir: "app/",
+        },
+    });
+    watch(['app/scss/**/*.scss'], compileStyles);
+    watch(['app/js/*.js', '!app/js/main.min.js'], script);
+    watch(['app/components/*', 'app/pages/*'], includePages);
+    watch(['app/images/**/*.*'], series(images, browserSync.reload));
+    watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+async function clean() {
+    return del.sync('build/', { force: true });
+}
+
+function building() {
+    return src([
+        'app/css/style.min.css',
+        'app/js/main.min.js',
+        'app/**/*.html',
+    ], { base: 'app' })
+        .pipe(dest('build'));
+}
+
+exports.compileStyles = compileStyles;
+exports.fonts = fonts;
+exports.images = images;
+exports.script = script;
+exports.includePages = includePages;
+exports.watching = watching;
+exports.clean = clean;
+exports.building = building;
+
+exports.default = parallel(compileStyles, fonts, script, includePages, watching);
+exports.build = series(clean, images, fonts, compileStyles, building);
+
+
